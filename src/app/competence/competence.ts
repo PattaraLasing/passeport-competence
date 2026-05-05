df96ace6490f8b7c83be43f8aa80b8ba9532e9b4
PattaraLasing <pattara.lasing@proton.me>
2026-05-05 10:25:19 +0200

    initial commit

---
+++ src/app/competence/competence.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competence',
+  imports: [],
+  templateUrl: './competence.html',
+  styleUrl: './competence.scss',
+})
+export class Competence {}
