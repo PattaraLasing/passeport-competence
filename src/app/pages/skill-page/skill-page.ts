011857b4d9c23ebc12ff28d860b316a22e95e424
PattaraLasing <pattara.lasing@proton.me>
2026-06-01 15:33:45 +0200

    add skill to firebase test ok

--- src/app/pages/skill-page/skill-page.ts
+++ src/app/pages/skill-page/skill-page.ts
@@ -1,9 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SkillService } from '../../services/skill/skill-service';
+import { IonContent, IonButton } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-skill-page',
+  imports: [IonButton, IonContent],
   templateUrl: './skill-page.html',
   styleUrls: ['./skill-page.scss'],
 })
 export class SkillPage  {
+
+  private readonly _skillService = inject(SkillService);
+
+  async handleAddSkill() {
+    //console.log('coucou');
+    
+    await this._skillService.addSkill({
+      uuid: 'uuid-test',
+      category: 'category-test',
+      title: 'my first skill',
+      description: 'this is my first skill to firebase'
+    })
+  }
 }
--- src/app/services/skill/skill-service.ts
+++ src/app/services/skill/skill-service.ts
@@ -1,6 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { doc, Firestore, setDoc } from '@angular/fire/firestore';
+import { Skill } from '../../interfaces/skill';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SkillService {}
+export class SkillService {
+
+  private readonly _fireStore = inject(Firestore);
+
+  async addSkill(skill: Skill) {
+    const id = uuidv4();
+    const docRef = doc(this._fireStore, 'skill-list/' + id);
+    await setDoc(docRef, skill).catch(error => {
+      console.log('error firebase : ' + error);   
+    });
+  }
+
+}
