fb975d95f5a3707e139077af1ab28ac87db11350
PattaraLasing <pattara.lasing@proton.me>
2026-06-01 11:42:42 +0200

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), 
-    provideRouter(routes), 
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
     provideIonicAngular({
       mode: 'md' //fixer en mode material design, sinon par défaut ionic prendra celui du système : ios ou md
     })
