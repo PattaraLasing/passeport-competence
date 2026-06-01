60fd1ab50590df84fa277add133fd2645cdd4c5f
PattaraLasing <pattara.lasing@proton.me>
2026-06-01 21:33:48 +0200

    displaying skill with card

--- src/app/pages/skill-page/skill-page.ts
+++ src/app/pages/skill-page/skill-page.ts
@@ -1,16 +1,19 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SkillService } from '../../services/skill/skill-service';
-import { IonContent, IonButton } from "@ionic/angular/standalone";
+import { IonContent, IonButton, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonGrid, IonRow, IonCol } from "@ionic/angular/standalone";
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-skill-page',
-  imports: [IonButton, IonContent],
+  imports: [AsyncPipe, IonCol, IonRow, IonGrid, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonContent],
   templateUrl: './skill-page.html',
   styleUrls: ['./skill-page.scss'],
 })
 export class SkillPage implements OnInit {
 
   private readonly _skillService = inject(SkillService);
+  
+  protected readonly skills = this._skillService.skill$;
 
   ngOnInit(): void {
     
