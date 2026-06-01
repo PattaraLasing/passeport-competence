dfaf1e63b10131ad288316d9a7a35786564d7345
PattaraLasing <pattara.lasing@proton.me>
2026-06-01 11:22:28 +0200

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'ch.pattaralasing.capacitordemo',
+    appName: 'capacitor-demo',
+    webDir: 'dist/capacitor-demo/browser',
+};
+
+export default config;
