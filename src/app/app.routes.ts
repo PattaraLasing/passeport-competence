0c4e5c9920fad97c501132cc648e5bdac7575c07
PattaraLasing <pattara.lasing@proton.me>
2026-06-07 20:24:17 +0200

    generate experience details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,31 +6,42 @@ export const routes: Routes = [
         path: '', redirectTo: '/vitrine', pathMatch: 'full'
     },
     {
-        path: 'vitrine', 
+        path: 'vitrine',
         loadComponent() {
             return import('./pages/vitrine-page/vitrine-page').then(m => m.VitrinePage);
         }
     },
     {
-        path: 'skill', 
+        path: 'skill',
         loadComponent() {
             return import('./pages/skill-page/skill-page').then(m => m.SkillPage);
         }
     },
     {
-        path: 'experience', 
-        loadComponent() {
-            return import('./pages/experience-page/experience-page').then(m => m.ExperiencePage);
-        }
+        path: 'experience',
+        children: [
+            {
+                path: '',
+                loadComponent() {
+                    return import('./pages/experience-page/experience-page').then(m => m.ExperiencePage);
+                }
+            },
+            {
+                path: ':uuid',
+                loadComponent() {
+                    return import('./pages/experience-details-page/experience-details-page').then(m => m.ExperienceDetailsPage);
+                }
+            }
+        ]
     },
     {
-        path: 'profil', 
+        path: 'profil',
         loadComponent() {
             return import('./pages/connexion-page/connexion-page').then(m => m.ConnexionPage);
         }
     },
     {
-        path: 'admin', 
+        path: 'admin',
         canActivate: [authGuard],
         loadComponent() {
             return import('./pages/admin-page/admin-page').then(m => m.AdminPage);
---
+++ src/app/pages/experience-details-page/experience-details-page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-details-page',
+  templateUrl: './experience-details-page.html',
+  styleUrls: ['./experience-details-page.scss'],
+})
+export class ExperienceDetailsPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
