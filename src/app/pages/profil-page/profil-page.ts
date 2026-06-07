6f5caa90036c68e90a1e87981dee24cc72b71ee5
PattaraLasing <pattara.lasing@proton.me>
2026-06-07 21:43:19 +0200

    generate profil page

---
+++ src/app/pages/profil-page/profil-page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-page',
+  templateUrl: './profil-page.html',
+  styleUrls: ['./profil-page.scss'],
+})
+export class ProfilPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
